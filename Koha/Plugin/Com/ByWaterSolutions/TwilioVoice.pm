69ebfa6d5b9bd33b5ed8e6eeb8a988f9fb387d4b
Kyle M Hall <kyle@bywatersolutions.com>
2020-05-13 10:47:45 -0400

    Initial Commit

---
+++ Koha/Plugin/Com/ByWaterSolutions/TwilioVoice.pm
@@ -0,0 +1,104 @@
+package Koha::Plugin::Com::ByWaterSolutions::TwilioVoice;
+
+use Modern::Perl;
+
+use base qw(Koha::Plugins::Base);
+
+use C4::Context;
+use C4::Auth;
+
+## Here we set our plugin version
+our $VERSION = "{VERSION}";
+our $MINIMUM_VERSION = "{MINIMUM_VERSION}";
+
+## Here is our metadata, some keys are required, some are optional
+our $metadata = {
+    name            => 'Twilio Voice Plugin',
+    author          => 'Kyle M Hall',
+    date_authored   => '2020-05-13',
+    date_updated    => "1900-01-01",
+    minimum_version => $MINIMUM_VERSION,
+    maximum_version => undef,
+    version         => $VERSION,
+    description     => 'This plugin enables sending of phone message to patrons via Twilio.',
+};
+
+sub new {
+    my ( $class, $args ) = @_;
+
+    ## We need to add our metadata here so our base class can access it
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    ## Here, we call the 'new' method for our base class
+    ## This runs some additional magic and checking
+    ## and returns our actual $self
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+sub configure {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    unless ( $cgi->param('save') ) {
+        my $template = $self->get_template({ file => 'configure.tt' });
+
+        ## Grab the values we already have for our settings, if any exist
+        $template->param(
+            enable_opac_payments => $self->retrieve_data('enable_opac_payments'),
+            foo             => $self->retrieve_data('foo'),
+            bar             => $self->retrieve_data('bar'),
+            last_upgraded   => $self->retrieve_data('last_upgraded'),
+        );
+
+        $self->output_html( $template->output() );
+    }
+    else {
+        $self->store_data(
+            {
+                enable_opac_payments => $cgi->param('enable_opac_payments'),
+                foo                => $cgi->param('foo'),
+                bar                => $cgi->param('bar'),
+                last_configured_by => C4::Context->userenv->{'number'},
+            }
+        );
+        $self->go_home();
+    }
+}
+
+sub install() {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+sub upgrade {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+sub uninstall() {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+sub api_routes {
+    my ( $self, $args ) = @_;
+
+    my $spec_str = $self->mbf_read('openapi.json');
+    my $spec     = decode_json($spec_str);
+
+    return $spec;
+}
+
+sub api_namespace {
+    my ( $self ) = @_;
+    
+    return 'twiliovoice';
+}
+
+1;
---
+++ README.md
@@ -0,0 +1,10 @@
+# Twilio Voice plugin for Koha
+
+This plugin allows Koha to send 'phone' notices via the Twilio service API.
+
+# Installation
+
+This plugin requires the installation of `WWW::Twilio::API` and `WWW::Twilio::TwiML` Perl modules on the Koha server before installation.
+If those modules are not install, this plugin may not appear in your Koha instance's list of installed plugins.
+
+# Configuration
