8026848b6684e68a7c0c566752943c38e3cdc309
Kyle M Hall <kyle@bywatersolutions.com>
2023-03-15 13:52:52 -0400

    Fix logic for type filter

--- Koha/Plugin/Com/ByWaterSolutions/TwilioVoice.pm
+++ Koha/Plugin/Com/ByWaterSolutions/TwilioVoice.pm
@@ -50,8 +50,8 @@ sub before_send_messages {
     my $letter_code = $params->{letter_code};
 
     # If a type limit is passed in, only run if the type is "phone"
-    return if ref($type) eq 'ARRAY' && !grep(/^phone$/, @$type); # 22.11.00, 22.05.8, 21.11.14 +, bug 27265
-    return if ref($type) eq q{}     && $type ne 'phone';
+    return if ref($type) eq 'ARRAY' && scalar @$type > 0 && !grep(/^phone$/, @$type); # 22.11.00, 22.05.8, 21.11.14 +, bug 27265
+    return if ref($type) eq q{}     && $type ne q{}      && $type ne 'phone';
 
     my $BorrowernumberFilter = $self->retrieve_data('BorrowernumberFilter');
 
