0c8fa155e5076e1e6ab26ea2252e2d92629fa699
Kyle M Hall <kyle@bywatersolutions.com>
2024-08-14 08:51:27 -0400

    Wrap all API methods in try/catch blocks

--- Koha/Plugin/Com/ByWaterSolutions/TwilioVoice/API.pm
+++ Koha/Plugin/Com/ByWaterSolutions/TwilioVoice/API.pm
@@ -36,105 +36,91 @@ use Koha::Notice::Messages;
 
 sub twiml {
     warn "Koha::Plugin::Com::ByWaterSolutions::TwilioVoice::API::twiml";
-    my $c = shift->openapi->valid_input or return;
+    return try {
+        my $c = shift->openapi->valid_input or return;
 
-    my $message_id = $c->validation->param('message_id');
-    my $message    = Koha::Notice::Messages->find($message_id);
-    unless ($message) {
-        return $c->render(
-            status  => 404,
-            openapi => { error => "Message not found." }
-        );
-    }
+        my $message_id = $c->validation->param('message_id');
+        my $message    = Koha::Notice::Messages->find($message_id);
+        unless ($message) {
+            return $c->render(
+                status  => 404,
+                openapi => { error => "Message not found." }
+            );
+        }
 
-=head
-    my $content = $message->content;
-    my $tw = new WWW::Twilio::TwiML;
-    $tw->Response
-    ->Pause({length => 2})->parent
-    ->Say(
-        {
-            voice    => "Polly.Joanna",
-            language => "en-US"
-        },
-        $content
-    )->parent
-    ->Pause({length => 2})->parent
-    ->Say(
-        {
-            voice    => "Polly.Joanna",
-            language => "en-US"
-        },
-        $message->content
-    );
-=cut
+        my $self = Koha::Plugin::Com::ByWaterSolutions::TwilioVoice->new({});
+        my $HoldMusicUrl
+          = $self->retrieve_data('HoldMusicUrl') || "http://com.twilio.music.classical.s3.amazonaws.com/ClockworkWaltz.mp3";
 
-    my $self = Koha::Plugin::Com::ByWaterSolutions::TwilioVoice->new({});
-    my $HoldMusicUrl
-      = $self->retrieve_data('HoldMusicUrl') || "http://com.twilio.music.classical.s3.amazonaws.com/ClockworkWaltz.mp3";
+        my $tw = new WWW::Twilio::TwiML;
+        $tw->Response->Pause({length => 2})->parent->Say({voice => "Polly.Joanna", language => "en-US"},
+            "You have a message from your library, please wait.")->parent->Pause({length => 2})
+          ->parent->Play($HoldMusicUrl);
 
-    my $tw = new WWW::Twilio::TwiML;
-    $tw->Response->Pause({length => 2})->parent->Say({voice => "Polly.Joanna", language => "en-US"},
-        "You have a message from your library, please wait.")->parent->Pause({length => 2})
-      ->parent->Play($HoldMusicUrl);
+        $message->status('sent');
+        $message->store();
 
-    $message->status('sent');
-    $message->store();
+        warn "TWILIO: twiml(): " . Data::Dumper::Dumper( $tw->to_string );
 
-    warn "TWILIO: twiml(): " . Data::Dumper::Dumper( $tw->to_string );
-
-    return $c->render( status => 200, format => "xml", text => $tw->to_string );
+        return $c->render( status => 200, format => "xml", text => $tw->to_string );
+    } catch {
+        $c->unhandled_exception($_);
+    };
 }
 
 sub update_message_status {
     warn "Koha::Plugin::Com::ByWaterSolutions::TwilioVoice::API::update_message_status";
-    my $c = shift->openapi->valid_input or return;
+    return try {
+        my $c = shift->openapi->valid_input or return;
 
-    my $message_id = $c->validation->param('message_id');
-    my $message    = Koha::Notice::Messages->find($message_id);
-    unless ($message) {
-        return $c->render(
-            status  => 404,
-            openapi => { error => "Message not found." }
-        );
-    }
+        my $message_id = $c->validation->param('message_id');
+        my $message    = Koha::Notice::Messages->find($message_id);
+        unless ($message) {
+            return $c->render(
+                status  => 404,
+                openapi => { error => "Message not found." }
+            );
+        }
 
-    my $body = $c->req->body;
+        my $body = $c->req->body;
 
-    if ( my %data = parse_urlencoded($body) ) {
-        my $twilio_status = $data{CallStatus};
+        if ( my %data = parse_urlencoded($body) ) {
+            my $twilio_status = $data{CallStatus};
 
-        warn "TWILIO: update_message_status(): "
-          . Data::Dumper::Dumper( \%data );
+            warn "TWILIO: update_message_status(): "
+              . Data::Dumper::Dumper( \%data );
 
-        my $status =
-          $twilio_status eq 'queued'
-          ? 'sent'
-          :            # We should get another status update later
-          $twilio_status eq 'ringing' ? 'sent' :    # Ditto
-          $twilio_status eq 'in-progress'
-          ? 'sent'
-          :    # The person picked up, basically completed
-          $twilio_status eq 'completed' ? 'sent' :    # Clearly completed
-          $twilio_status eq 'busy'
-          ? 'failed'
-          :    # TODO: Make retrying busy a plugin setting
-          $twilio_status eq 'failed'
-          ? 'failed'
-          :    # Phone number was most likely invalid
-          $twilio_status eq 'no-answer' ? 'failed' :    # See TODO above
-          'failed';    # Staus was something we didn't expect
-        $message->status($status);
-        $message->store();
+            my $status =
+              $twilio_status eq 'queued'
+              ? 'sent'
+              :            # We should get another status update later
+              $twilio_status eq 'ringing' ? 'sent' :    # Ditto
+              $twilio_status eq 'in-progress'
+              ? 'sent'
+              :    # The person picked up, basically completed
+              $twilio_status eq 'completed' ? 'sent' :    # Clearly completed
+              $twilio_status eq 'busy'
+              ? 'failed'
+              :    # TODO: Make retrying busy a plugin setting
+              $twilio_status eq 'failed'
+              ? 'failed'
+              :    # Phone number was most likely invalid
+              $twilio_status eq 'no-answer' ? 'failed' :    # See TODO above
+              'failed';    # Staus was something we didn't expect
+            $message->status($status);
+            $message->store();
 
-        return $c->render( status => 204, text => q{} );
-    }
-    else {
-        return $c->render(
-            status  => 501,
-            openapi => { error => "Unable to decode json" }
-        );
-    }
+            return $c->render( status => 204, text => q{} );
+        }
+        else {
+            return $c->render(
+                status  => 501,
+                openapi => { error => "Unable to decode json" }
+            );
+        }
+    } catch {
+        $c->unhandled_exception($_);
+    };
 }
 
 sub amd_callback {
